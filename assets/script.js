49fa74528df50a0c00b5b9022d29fdd275f943e8
EZaiger <elisabeth.zaiger@oit.edu>
2022-07-08 16:03:11 -0700

    detailed psuedocode and js global variable entry

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,33 @@
 // Global Variables
+var searchFormEl = document.querySelector(".form");
+var searchButtonEl = document.querySelector(".search-btn");
+var historyButtonEl = document.querySelector(".history.btn");
+var weatherResults = document.querySelector(".weather-results");
+var currentConditions = document.querySelector(".current-conditions");
 
 // Function to search for a city
+var citySearch = function () {
+    // obtain input from user
+    // event listener for the search button click
+    // use weather API to find entered city
+}
+
+// Function to display history of searched cities
+    // button generated showing searched city
+    // newest search appears at top
+    // searched city stored in local storage
 
 // Function to display current weather conditions for that city
+    // display weather conditions in .weather-results
+    // format city, date and insert icon
+    // retrieve top, wind, humidity and UV index from API
 
 // Function to determine if UV index is favorable, moderate, or severe
 
-// Function to display future weather conditions for that city
+// Function to display future weather conditions for that city (5 day forcast)
+    // create a card for each day with date, icon, temp, wind and humidity
 
-// Function to store current/future weather conditions for previously searched cities
+// Function to store weather conditions for searched cities
+
+// Function to update local storage with previously searched cities
+
