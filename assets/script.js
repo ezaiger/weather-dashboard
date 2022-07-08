23ccf11c4275f795b3c86f3e91740e17241ead30
EZaiger <elisabeth.zaiger@oit.edu>
2022-07-08 14:48:31 -0700

    HTML setup, general psuedocode

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// Global Variables
+
+// Function to search for a city
+
+// Function to display current weather conditions for that city
+
+// Function to determine if UV index is favorable, moderate, or severe
+
+// Function to display future weather conditions for that city
+
+// Function to store current/future weather conditions for previously searched cities
