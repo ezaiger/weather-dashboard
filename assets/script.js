09d9d73465a53266d552461b9b0fb3840d5f13dc
EZaiger <elisabeth.zaiger@oit.edu>
2022-08-07 21:44:42 -0700

    current weather forecast

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,20 @@ var cityInputEl = document.querySelector(".city-input");
 var searchFormEl = document.querySelector(".form");
 var searchButtonEl = document.querySelector(".search-btn");
 var historyButtonEl = document.querySelector(".history.btn");
+var cityResultEl = document.querySelector(".city-result");
+
+// Current Forecast Variables
+var tempForecastEl = document.querySelector(".temp");
+var windForecastEl = document.querySelector(".wind");
+var humidityForecastEl = document.querySelector(".humidity");
+var uviForecastEl = document.querySelector(".uv-index")
+// Five Day Forecast Variables (FDF)
+
 var weatherResultsEl = document.querySelector(".weather-results");
 var currentConditionsEl = document.querySelector(".current-conditions");
 var weatherAPIKey = "faaae616638a9dcb6e179054b7709009";
 
-// Function to use weather API to find entered city
+// Function to populate entered city information
 var weatherSearch = function (cityName) {
     var urlCurrentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherAPIKey}&units=imperial`;
     // fetches current weather
@@ -25,6 +34,15 @@ var weatherSearch = function (cityName) {
                 })
                 .then(function (fivedayData) {
                     console.log(fivedayData)
+                    // current weather information
+                    cityResultEl.innerHTML = currentData.name + moment(currentData.dt, 'X').format(" (MM/DD/YYYY) ") + `<img class="w-2"
+                    src="http://openweathermap.org/img/wn/${currentData.weather[0].icon}@2x.png" />`;
+                    tempForecastEl.innerHTML = "Temp: " + fivedayData.current.temp;
+                    windForecastEl.innerHTML = "Wind: " + fivedayData.current.wind_speed;
+                    humidityForecastEl.innerHTML = "Humidity: " + fivedayData.current.humidity;
+                    uviForecastEl.innerHTML = "UV Index: " + fivedayData.current.uvi;
+                    // five day weather information
+
                 })
         })
 };
