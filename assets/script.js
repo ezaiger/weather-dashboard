c1dc576e516eb68e75f39a9c1af5856086bb1428
EZaiger <elisabeth.zaiger@oit.edu>
2022-08-21 21:24:58 -0700

    uv and history button setup/css

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,16 @@
 var cityInputEl = document.querySelector(".city-input");
 var searchFormEl = document.querySelector(".form");
 var searchButtonEl = document.querySelector(".search-btn");
-var historyButtonEl = document.querySelector(".history.btn");
 var cityResultEl = document.querySelector(".city-result");
 var fiveDayEl = document.querySelector("#fiveDay");
-var searchedCityEl = document.querySelector("#searchedCity")
+var searchedCityEl = document.querySelector("#searchedCity");
+var uvIndexEl = document.querySelector("#uv-condition");
 
 // Current Forecast Variables
 var tempForecastEl = document.querySelector(".temp");
 var windForecastEl = document.querySelector(".wind");
 var humidityForecastEl = document.querySelector(".humidity");
-var uviForecastEl = document.querySelector(".uv-index")
+var uviForecastEl = document.querySelector(".uv-index");
 var weatherResultsEl = document.querySelector(".weather-results");
 var currentConditionsEl = document.querySelector(".current-conditions");
 var weatherAPIKey = "faaae616638a9dcb6e179054b7709009";
@@ -19,93 +19,107 @@ var cityList = [];
 
 // Function to populate entered city information
 var weatherSearch = function (cityName) {
-    var urlCurrentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherAPIKey}&units=imperial`;
-    // fetches current weather
-    fetch(urlCurrentWeather)
+  var urlCurrentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherAPIKey}&units=imperial`;
+  // fetches current weather
+  fetch(urlCurrentWeather)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (currentData) {
+      console.log(currentData);
+      var urlOneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&exclude={part}&appid=${weatherAPIKey}&units=imperial`;
+      // fetches 7 day forecast
+      fetch(urlOneCall)
         .then(function (response) {
-            return response.json()
+          return response.json();
         })
-        .then(function (currentData) {
-            console.log(currentData)
-            var urlOneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&exclude={part}&appid=${weatherAPIKey}&units=imperial`
-            // fetches 7 day forecast
-            fetch(urlOneCall)
-                .then(function (response) {
-                    return response.json()
-                })
-                .then(function (fivedayData) {
-                    console.log(fivedayData)
-                    // add entered city into local storage
-                    cityList.push(currentData.name);
-                    localStorage.setItem("cityList", JSON.stringify(cityList));
-                    populateCity();
-                    // populates current city weather info
-                    cityResultEl.innerHTML = currentData.name + moment(currentData.dt, 'X').format(" (MM/DD/YYYY) ") + `<img class="w-2"
+        .then(function (fivedayData) {
+          console.log(fivedayData);
+          // add entered city into local storage
+          cityList.push(currentData.name);
+          localStorage.setItem("cityList", JSON.stringify(cityList));
+          populateCity();
+          // populates current city weather info
+          cityResultEl.innerHTML =
+            currentData.name +
+            moment(currentData.dt, "X").format(" (MM/DD/YYYY) ") +
+            `<img class="w-2"
                     src="http://openweathermap.org/img/wn/${currentData.weather[0].icon}@2x.png" />`;
-                    tempForecastEl.innerHTML = "Temp: " + fivedayData.current.temp;
-                    windForecastEl.innerHTML = "Wind: " + fivedayData.current.wind_speed;
-                    humidityForecastEl.innerHTML = "Humidity: " + fivedayData.current.humidity;
-                    uviForecastEl.innerHTML = "UV Index: " + fivedayData.current.uvi;
-                // populates current city 5 day forecast weater
-                fiveDayEl.innerHTML = "";
-                var card = "";
-                for (let i = 1; i < 6; i++) {
-                    card = card + `<div class="col-sm-2">
+          tempForecastEl.innerHTML = "Temp: " + fivedayData.current.temp + "°F";
+          windForecastEl.innerHTML = "Wind: " + fivedayData.current.wind_speed;
+          humidityForecastEl.innerHTML =
+            "Humidity: " + fivedayData.current.humidity;
+          var uv = fivedayData.current.uvi;
+          uvIndexEl.innerHTML = fivedayData.current.uvi;
+
+          if (uv < 3) {
+            uvIndexEl.classList.add("green");
+          } else if (uv < 5) {
+            uvIndexEl.classList.add("orange");
+          } else {
+            uvIndexEl.classList.add("red");
+          }
+
+          // populates current city 5 day forecast weather
+          fiveDayEl.innerHTML = "";
+          var card = "";
+          for (let i = 1; i < 6; i++) {
+            console.log(fivedayData.daily[i].weather[0].icon);
+            card =
+              card +
+              `<div class="col-sm-2">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">${moment(fivedayData.daily[i].dt, 'X').format("MM/DD/YYYY")}</h5>
-                            <img class="w-50" src="http://openweathermap.org/img/wn/${fivedayData.daily[i].weather[0].icon}@2x.png" />
-                            <p class="card-text">Temp: ${fivedayData.daily[i].temp.day}</p>
-                            <p class="card-text">Wind: ${fivedayData.daily[i].wind_speed}</p>
-                            <p class="card-text">Humidity: ${fivedayData.daily[i].humidity}</p>
+                            <h5 class="card-title">${moment(
+                fivedayData.daily[i].dt,
+                "X"
+              ).format("MM/DD/YYYY")}</h5>
+                            <img class="w-50" src="http://openweathermap.org/img/wn/${fivedayData.daily[i].weather[0].icon
+              }@2x.png" />
+                            <p class="card-text">Temp: ${fivedayData.daily[i].temp.day
+              }°F</p>
+                            <p class="card-text">Wind: ${fivedayData.daily[i].wind_speed
+              }</p>
+                            <p class="card-text">Humidity: ${fivedayData.daily[i].humidity
+              }</p>
                         </div>
                     </div>
-                </div>`
-                }
-                fiveDayEl.innerHTML = card;
-                })
-        })
+                </div>`;
+          }
+          fiveDayEl.innerHTML = card;
+        });
+    });
 };
 
 // Function to create history button for previously searched city
 var populateCity = function () {
-    var cityData = JSON.parse(localStorage.getItem("cityList"));
-    if (cityData) {
-        cityList = cityData
-        searchedCityEl.innerHTML = "";
-    for (i=0; i < cityList.length; i++) {
-        searchedCityEl.innerHTML += `<li class="list-group-item border-0"><button class="btn history-btn w-100"
-        type="button">${cityList[i]}</button></li>`
+  var cityData = JSON.parse(localStorage.getItem("cityList"));
+  if (cityData) {
+    cityList = cityData;
+    searchedCityEl.innerHTML = "";
+    for (i = 0; i < cityList.length; i++) {
+      searchedCityEl.innerHTML += `<li class="list-group-item border-0"><button class="btn history-btn w-100"
+        type="button">${cityList[i]}</button></li>`;
     }
-    }
+  }
+  var historyButtonEl = document.querySelectorAll(".history-btn");
+  for (i = 0; i < historyButtonEl.length; i++) {
+    historyButtonEl[i].addEventListener("click", showCity);
+  }
 };
 populateCity();
 
+// Function to repeat search on previously searched city
+var showCity = function () {
+  weatherSearch(this.textContent);
+};
+
 // Function to search for a city
 var citySearch = function () {
-    // obtain input from user
-    var cityName = cityInputEl.value;
-    weatherSearch(cityName);
+  // obtain input from user
+  var cityName = cityInputEl.value;
+  weatherSearch(cityName);
 };
+
 // event listener for the search button click
 searchButtonEl.addEventListener("click", citySearch);
-
-// Function to display history of searched cities
-    // button generated showing searched city
-    // newest search appears at top
-    // searched city stored in local storage
-
-// Function to display current weather conditions for that city
-    // display weather conditions in .weather-results
-    // format city, date and insert icon
-    // retrieve top, wind, humidity and UV index from API
-
-// Function to determine if UV index is favorable, moderate, or severe
-
-// Function to display future weather conditions for that city (5 day forcast)
-    // create a card for each day with date, icon, temp, wind and humidity
-
-// Function to store weather conditions for searched cities
-
-// Function to update local storage with previously searched cities
-
